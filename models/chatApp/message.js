d48bbdac8031e3563b454d636b9dc576ecb4578f
Admin <janviermbilizi@gmail.com>
2020-10-24 13:39:43 -0400

    created a schema for a single direct message

---
+++ models/chatApp/message.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
+
+var MessageSchema = new mongoose.Schema({
+  sender: {
+    type: ObjectId,
+    ref: "User",
+  },
+  receivers: [
+    {
+      receiver: {
+        type: ObjectId,
+        ref: "User",
+      },
+      read: { type: Boolean, default: false },
+    },
+  ],
+  content: String,
+  time: {
+    type: Date,
+    default: Date.now,
+  },
+  isRight: { type: Boolean, default: true },
+  delivered: { type: Boolean, default: false },
+
+  is_group_message: { type: Boolean, default: false },
+  likes: [{ type: ObjectId, ref: "User" }],
+});
+
+module.exports = mongoose.model("Message", MessageSchema);
