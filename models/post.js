6fe44de2f11f134725ab63238fe66c8c002c7494
Admin <janviermbilizi@gmail.com>
2020-11-06 22:12:07 -0500

    changed photo to file and added aws params

--- models/post.js
+++ models/post.js
@@ -1,36 +1,38 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { ObjectId } = mongoose.Schema;
 
 const postSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true
+  title: {
+    type: String,
+    required: true,
+  },
+  body: {
+    type: String,
+    required: true,
+  },
+  file: {
+    url: String,
+    key: String,
+    name: String,
+    contentType: String,
+  },
+  postedBy: {
+    type: ObjectId,
+    ref: "User",
+  },
+  created: {
+    type: Date,
+    default: Date.now,
+  },
+  updated: Date,
+  likes: [{ type: ObjectId, ref: "User" }],
+  comments: [
+    {
+      text: String,
+      created: { type: Date, default: Date.now },
+      postedBy: { type: ObjectId, ref: "User" },
     },
-    body: {
-        type: String,
-        required: true
-    },
-    photo: {
-        data: Buffer,
-        contenType: String
-    },
-    postedBy: {
-        type: ObjectId,
-        ref: 'User'
-    },
-    created: {
-        type: Date,
-        default: Date.now
-    },
-    updated: Date,
-    likes: [{ type: ObjectId, ref: 'User' }],
-    comments: [
-        {
-            text: String,
-            created: { type: Date, default: Date.now },
-            postedBy: { type: ObjectId, ref: 'User' }
-        }
-    ]
+  ],
 });
 
-module.exports = mongoose.model('Post', postSchema);
+module.exports = mongoose.model("Post", postSchema);
