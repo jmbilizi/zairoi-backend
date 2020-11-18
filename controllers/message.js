6eb834bf79a7da4164c9f8baf8c90f5f24c0cb9f
Admin <janviermbilizi@gmail.com>
2020-11-18 14:16:35 -0500

    added io emit and chat

--- controllers/message.js
+++ controllers/message.js
@@ -1,4 +1,5 @@
-const Message = require("../models/chatApp/message");
+const Message = require("../models/message");
+const Conversation = require("../models/chat");
 const mongoose = require("mongoose");
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
@@ -9,7 +10,9 @@ exports.messageById = (req, res, next, id) => {
   Message.findById(id)
     .populate("sender", "_id name role photo")
     .populate("receivers.receiver", "_id name role photo")
-    .select("_id chat sender receivers body time delivered is_group_message likes")
+    .select(
+      "_id chat sender receivers body time delivered is_group_message likes"
+    )
     .exec((err, message) => {
       if (err || !message) {
         return res.status(400).json({
@@ -33,37 +36,65 @@ exports.getMessages = async (req, res) => {
 };
 
 exports.createMessage = (req, res, next) => {
-  let form = new formidable.IncomingForm();
-  form.keepExtensions = true;
-  form.parse(req, (err, fields, files) => {
-    if (err) {
-      return res.status(400).json({
-        error: "file could not be uploaded",
-      });
-    }
-    const message = new Message(fields);
-    let data = message.data;
+  let from = req.profile;
+  let to = mongoose.Types.ObjectId(req.body.receivers);
 
-    req.profile.hashed_password = undefined;
-    req.profile.salt = undefined;
-    message.sender = req.profile;
-
-    if (files.file) {
-      for (let i = 0; i < data.length; i++) {
-        data[i].type = fs.readFileSync(files.file.path);
-        data[i].content = files.file.type;
-      }
-    }
-
-    message.save((err, result) => {
+  Conversation.findOneAndUpdate(
+    {
+      recipients: {
+        $all: [{ $elemMatch: { $eq: from } }, { $elemMatch: { $eq: to } }],
+      },
+    },
+    {
+      recipients: [req.profile, req.body.receivers],
+      lastMessage: req.body.body,
+      time: Date.now(),
+    },
+    { upsert: true, new: true, setDefaultsOnInsert: true },
+    function (err, conversation) {
       if (err) {
-        return res.status(400).json({
-          error: err,
+        console.log(err);
+        res.setHeader("Content-Type", "application/json");
+        res.end(JSON.stringify({ message: "Failure" }));
+        res.sendStatus(500);
+      } else {
+        let form = new formidable.IncomingForm();
+        form.keepExtensions = true;
+        form.parse(req, (err, fields, files) => {
+          if (err) {
+            return res.status(400).json({
+              error: "file could not be uploaded",
+            });
+          }
+          const message = new Message(fields);
+          let data = message.data;
+
+          req.profile.hashed_password = undefined;
+          req.profile.salt = undefined;
+          message.sender = req.profile;
+
+          //emit with io
+          req.io.sockets.emit("messages", req.body.body);
+
+          if (files.file) {
+            for (let i = 0; i < data.length; i++) {
+              data[i].type = fs.readFileSync(files.file.path);
+              data[i].content = files.file.type;
+            }
+          }
+
+          message.save((err, result) => {
+            if (err) {
+              return res.status(400).json({
+                error: err,
+              });
+            }
+            res.json(result);
+          });
         });
       }
-      res.json(result);
-    });
-  });
+    }
+  );
 };
 
 exports.deleteMessage = (req, res) => {
